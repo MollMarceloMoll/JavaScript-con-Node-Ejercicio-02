5a2992ffa64c10012b38f1e79807d1425a4204f2
Marcelo Moll <75146662+MollMarceloMoll@users.noreply.github.com>
2023-01-17 12:35:40 -0300

    Add files via upload

---
+++ ejercicio02.js
@@ -0,0 +1,20 @@
+// Nombre
+const nombre = "Marcelo";
+// Edad
+const edad = 30;
+// Desarrollador
+const desarrollador = true;
+// Fecha de nacimiento
+const fecha = new Date(1992, 2, 9);
+// Libro favorito
+const libro = {
+    titulo: "Aprende A Programar Con Kotlin",
+    autor: "LUJÁN CASTILLO, José Dimas",
+    fecha: "20-09-2017",
+    url: "https://www.todostuslibros.com/libros/aprende-a-programar-con-kotlin_978-84-947170-2-4#synopsis",
+};
+console.log(nombre);
+console.log(edad);
+console.log(desarrollador);
+console.log(fecha);
+console.log(libro);
